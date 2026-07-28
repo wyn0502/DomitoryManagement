7b54d9dec7739a2eaa4397e2a8ddb67574dfdc70
Đặng Chấn Đông <24107609@st.phenikaa-uni.edu.vn>
2026-07-28 18:13:44 +0000

    sua loi khong chay duoc server

--- server/src/buildings/buildings.module.ts
+++ server/src/buildings/buildings.module.ts
@@ -1,12 +1,22 @@
-import {Module} from '@nestjs/common';
-import {TypeOrmModule} from '@nestjs/typeorm';
-import {BuildingsService} from './buildings.service';
-import {BuildingsController} from './buildings.controller';
-import {Building} from './entities/building.entity';
+import { Module } from '@nestjs/common';
+import { BuildingsService } from './buildings.service';
+import { BuildingsController } from './buildings.controller';
+import { DatabaseModule } from '../database/database.module'; 
+import { DataSource } from 'typeorm'; // Hoặc thư viện DB bạn đang dùng
+import { Building } from './entities/building.entity'; // 👈 Thay bằng đường dẫn tới Entity Building của bạn
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Building])],
+  imports: [DatabaseModule], 
   controllers: [BuildingsController],
-  providers: [BuildingsService],
+  providers: [
+    BuildingsService,
+    // 👈 THÊM ĐOẠN PROVIDER NÀY VÀO ĐÂY:
+    {
+      provide: 'BuildingRepository',
+      useFactory: (dataSource: DataSource) => dataSource.getRepository(Building),
+      inject: ['DATA_SOURCE'], // Lấy DATA_SOURCE được export từ DatabaseModule
+    },
+  ],
+  exports: [BuildingsService],
 })
 export class BuildingsModule {}
